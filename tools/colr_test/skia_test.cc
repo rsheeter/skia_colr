abfde62b9c6c2b60eaebac7951def9942d92901c
rsheeter <rsheeter@google.com>
2020-07-09 22:27:36 -0700

    WIP add shaping so we can render arbitrary strings

--- tools/colr_test/skia_test.cc
+++ tools/colr_test/skia_test.cc
@@ -16,48 +16,119 @@
 
 #include "tools/flags/CommandLineFlags.h"
 
+#include "hb-ot.h"
+
 #include <iterator>
 #include <string>
 
 static DEFINE_string(font, "skia_test/samples-colr_1.ttf", "Font to load.");
-static DEFINE_string(text, "duck", "Text to render in font.");
+static DEFINE_string(text, "simple_linear", "Text to render in font.");
 static DEFINE_string(output, "colr_v1_glyph.png", "File to write.");
 
-const uint16_t kTestGlyphIds[] = {13, 14};
-// const char kTestFileName[] = "skia_test/noto-emoji-glyf_colr_1.ttf";
-// const uint16_t kTestGlyphIds[] = {6,  10, 14, 17, 18, 26, 31, 32,
-//                                   35, 37, 38, 41, 43, 53, 54};
+const double FONT_SIZE_SCALE = 64.0f;
 const float kFontSize = 128;
+const float kMargin = 32;
 
 int main(int argc, char** argv) {
   CommandLineFlags::Parse(argc, argv);
 
+  auto font_mgr = sk_sp<SkFontMgr>(SkFontMgr_New_Custom_Empty());
+
+  // Shape & thus measure our text
+  // Initially based on https://harfbuzz.github.io/ch03s03.html
+  // until that turned out to be insufficient to compile.
+  // Then based on https://github.com/aam/skiaex/blob/master/app/main.cpp,
+  // which is referenced from Skia faq about how to get shaping.
+
+  auto data = SkData::MakeFromFileName(FLAGS_font[0]);
+  std::unique_ptr<SkStreamAsset> data_stream(new SkMemoryStream(data));
+  sk_sp<SkTypeface> face(
+      font_mgr->makeFromStream(std::move(data_stream)));
+  SkASSERT(face);
+
+
+  SkFont font(face);
+  font.setSize(kFontSize);
+
+  // get a blob of our font file
+  auto destroy = [](void *d) { static_cast<SkData*>(d)->unref(); };
+  const char* bytes = (const char*)data->data();
+  unsigned int size = (unsigned int)data->size();
+  hb_blob_t* hb_font_blob = hb_blob_create(bytes,
+                                           size,
+                                           HB_MEMORY_MODE_READONLY,
+                                           data.release(),
+                                           destroy);
+  hb_blob_make_immutable(hb_font_blob);
+
+  // We'll need an hb face & font to shape
+  hb_face_t* hb_face = hb_face_create(hb_font_blob, /* index */ 0);
+  hb_blob_destroy(hb_font_blob);
+  hb_font_t *hb_font = hb_font_create(hb_face);
+
+  // Let's all agree on upem
+  hb_face_set_upem(hb_face, face->getUnitsPerEm());
+
+  // Oh wise and powerful HarfBuzz, what glyphs and where?
+  hb_buffer_t *hb_buffer = hb_buffer_create ();
+  hb_buffer_add_utf8 (hb_buffer, FLAGS_text[0], -1, 0, -1);
+  hb_buffer_guess_segment_properties (hb_buffer);
+
+  hb_shape (hb_font, hb_buffer, NULL, 0);
+
+  // Stand back, I'm thinking about drawing
+  SkTextBlobBuilder textBlobBuilder;
+  unsigned len = hb_buffer_get_length (hb_buffer);
+
+  hb_glyph_info_t *info = hb_buffer_get_glyph_infos (hb_buffer, NULL);
+  hb_glyph_position_t *pos = hb_buffer_get_glyph_positions (hb_buffer, NULL);
+  auto runBuffer = textBlobBuilder.allocRunPos(font, len);
+
+  double x = kMargin;
+  double y = kMargin;
+  for (unsigned int i = 0; i < len; i++)
+  {
+    runBuffer.glyphs[i] = info[i].codepoint;
+    printf("gid %d (%.01f %.01f)\n", info[i].codepoint, x, y);
+    reinterpret_cast<SkPoint*>(runBuffer.pos)[i] = SkPoint::Make(
+      x + pos[i].x_offset / FONT_SIZE_SCALE,
+      y - pos[i].y_offset / FONT_SIZE_SCALE);
+
+    x += pos[i].x_advance / FONT_SIZE_SCALE;
+    y += pos[i].y_advance / FONT_SIZE_SCALE;
+  }
+  x += kMargin;
+  y += kMargin;
+
+  // Let's paint something!
+  printf("Making %.1f x %.1f canvas\n", x, y);
   SkImageInfo image_info =
-      SkImageInfo::Make(2000, 200, SkColorType::kRGB_565_SkColorType,
+      SkImageInfo::Make(x, y, SkColorType::kRGB_565_SkColorType,
                         SkAlphaType::kOpaque_SkAlphaType);
   sk_sp<SkSurface> surface(SkSurface::MakeRaster(image_info));
   if (!surface)
     return 1;
   SkCanvas* canvas = surface->getCanvas();
   canvas->clear(SK_ColorWHITE);
+
   SkPaint paint;
 
-  auto font_mgr = sk_sp<SkFontMgr>(SkFontMgr_New_Custom_Empty());
-  auto font_file_stream = SkStream::MakeFromFile(FLAGS_font[0]);
-  sk_sp<SkTypeface> typeface(
-      font_mgr->makeFromStream(std::move(font_file_stream)));
+  // Example suggest we should set text encoding but .. no such api?
 
-  SkASSERT(typeface);
-
-  SkFont font(typeface);
-  font.setSize(kFontSize);
-
+  canvas->drawTextBlob(textBlobBuilder.make(), 0, 0, paint);
+  /*
   for (size_t i = 0; i < sizeof(kTestGlyphIds) / sizeof(uint16_t); ++i) {
     canvas->drawSimpleText(&kTestGlyphIds[i], 2, SkTextEncoding::kGlyphID,
                            10 + kFontSize * (i), kFontSize + 10, font, paint);
   }
+  */
 
   sk_sp<SkImage> image = surface->makeImageSnapshot();
   sk_sp<SkData> png = image->encodeToData(SkEncodedImageFormat::kPNG, 100);
   SkFILEWStream{FLAGS_output[0]}.write(png->data(), png->size());
+
+  // Cleanup
+  hb_buffer_destroy (hb_buffer);
+  hb_font_destroy(hb_font);
+  hb_face_destroy(hb_face);
 }
